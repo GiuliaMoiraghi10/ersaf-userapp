38d43a0304b48882bc669101720d8676754c1922
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-09-29 15:49:22 +0200

    Install dependences

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ function App() {
 
   return (
     <>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+        <h1>App Users</h1>
+      </div>
     </>
   )
 }
