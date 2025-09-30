e0d4f542b2e04ac4204bac07e9d014bba3a90317
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-09-30 23:06:28 +0200

    Correggo footer image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
+import foglieImage from '../assets/foglie.jpg';
+
 export default function Footer() {
     return (
-        <footer className="w-full h-20 md:h-24 bg-cover bg-center bg-no-repeat mt-8"
-            style={{ backgroundImage: "url('/src/assets/foglie.jpg')" }}>
+        <footer
+            className="w-full h-20 md:h-24 bg-cover bg-center bg-no-repeat mt-8"
+            style={{ backgroundImage: `url(${foglieImage})` }}
+        >
             <div className="w-full h-full bg-black/20"></div>
         </footer>
     );
